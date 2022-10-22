0594e4ae610027d8ba1dbcb0872033e62c54fc56
mark48853 <sathanalao@hotmail.com>
2022-10-22 23:54:13 +0700

    change popup ui

--- modules/professor.js
+++ modules/professor.js
@@ -11,6 +11,7 @@ const removeProfessor = () => {
 
 const createProfessor = (quote) => {
 
+    let bigDiv = document.createElement('div');
     let div = document.createElement('div');
     let figure = document.createElement('img');
     let h1 = document.createElement('H1');
@@ -21,17 +22,18 @@ const createProfessor = (quote) => {
     h1.style['font-size'] = '30px';
     h1.style['font-style'] = 'italic';
     h1.style['font-family'] = 'sans-serif';
+    h1.style['-webkit-text-stroke'] = '1px white';
     h1.classList.add("ajarn-jong-ja-chuay-khun");
 
     figure.src = "https://avatars.githubusercontent.com/u/13951290?v=4";
     figure.style.position = 'fixed';
-    figure.style.left = '0px';
-    figure.style.bottom = '0px';
+    figure.style.top = '0px';
+    figure.style.right = '0px';
     figure.style.zIndex = 6968;
     figure.style.padding = '50px';
     figure.style.opacity = 1;
-    figure.style.height = "auto";
-    figure.style.width = "400px";
+    figure.style.height = "400px";
+    figure.style.width = "auto";
     figure.style.filter = "blur(1px)brightness(169%)contrast(169%)";
     figure.setAttribute('rel', 'bottom');
     figure.classList.add("ajarn-jong-ja-chuay-khun");
@@ -39,14 +41,19 @@ const createProfessor = (quote) => {
     div.setAttribute('rel', 'bottom');
     div.style.position = 'fixed';
     div.classList.add("ajarn-jong-ja-chuay-khun");
-    div.style.left = '0px';
-    div.style.bottom = '0px';
+    div.style.top = '300px';
+    div.style.right = '50px';
     div.style.zIndex = 6969;
     div.style.padding = '50px';
+    div.style.float = 'top';
+    div.appendChild(figure);
     div.appendChild(h1);
 
-    document.body.appendChild(figure);
-    document.body.appendChild(div);
+    bigDiv.appendChild(figure);
+    bigDiv.appendChild(div);
+    bigDiv.style.top = '0px';
+    bigDiv.style.right = '0px';
+    document.body.appendChild(bigDiv);
 
     const timeOut = setTimeout(removeProfessor, 3569);
 }
